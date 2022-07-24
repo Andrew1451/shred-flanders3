a68c13b6132fb70d17e28a6a4b191c14bbd15d4b
Andrew1451 <44391620+Andrew1451@users.noreply.github.com>
2022-07-24 16:30:09 -0400

    profile

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="flex justify-between w-10/12 h-16 mx-auto my-10 border border-black">
+    <nav class="self-center w-5/12">
+      <ul class="flex">
+        <li class="mr-8 text-white cursor-pointer">Portfolio</li>
+        <li class="mr-8 text-white cursor-pointer">About Me</li>
+        <li class="mr-8 text-white cursor-pointer">Get In Touch</li>
+      </ul>
+    </nav>
+    <img src="./assets/hello-logo.png" class="self-center mx-auto w-14 h-14" />
+    <div class="flex justify-end w-5/12 h-16">
+      <div class="my-auto cursor-pointer ml-7">
+        <img src="./assets/github.png" />
+      </div>
+      <div class="my-auto cursor-pointer ml-7">
+        <img src="./assets/insta.png" />
+      </div>
+      <div class="my-auto cursor-pointer ml-7">
+        <img src="./assets/twitter.png" />
+      </div>
+      <div class="my-auto cursor-pointer ml-7">
+        <img src="./assets/linkedin.png" />
+      </div>
+    </div>
   </div>
+  <header>
+    <div class="w-10/12 mx-auto my-24 text-center">
+      <h1 class="text-[3rem] font-medium text-white">Shred Flanders</h1>
+      <h2 class="text-[3rem] text-green-400">Guitar Shredder</h2>
+      <h3 class="text-[3rem] text-white">Web Developer</h3>
+      <div class="mx-auto my-12">
+        <img src="./assets/Shred Flanders.png" class="mx-auto border-4 border-green-400 w-6/6 h-96" />
+      </div>
+    </div>
+  </header>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
