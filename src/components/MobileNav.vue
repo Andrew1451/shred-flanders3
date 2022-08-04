11e771c117a2b88692cd50308ab8be2558a69999
Andrew1451 <44391620+Andrew1451@users.noreply.github.com>
2022-08-03 20:56:16 -0400

    close navigation on page change

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lg:hidden">
-    <router-link class="mx-auto" to="/">
-      <img src="@/assets/Hi-di Ho.png" class="w-24 h-24 mx-auto my-7" />
+    <router-link class="block w-24 mx-auto my-7" to="/">
+      <img src="@/assets/Hi-di Ho.png" class="w-24 h-24" />
     </router-link>
     <div class="absolute top-0 z-10 w-12 h-12 right-3" @click="toggleMenu">
         <span class="inline-block w-8 h-0.5 absolute rounded-sm top-7 bg-white" :class="menuOpen ? 'hidden' : 'inline-block'"></span>
@@ -12,7 +12,7 @@
     <div class="absolute top-0 right-0 w-48 overflow-hidden h-96">
       <div class="absolute flex flex-col justify-between w-48 transition duration-500 bg-black border border-white h-96 -right-100" :class="menuOpen ? '-translate-x-0' : 'translate-x-full' ">
         <nav class="self-center">
-          <ul class="flex flex-col pt-5">
+          <ul class="flex flex-col pt-5" @click="closeMenu">
             <li class="my-2 mr-8 text-white cursor-pointer"><router-link to="/">Portfolio</router-link></li>
             <li class="my-2 mr-8 text-white cursor-pointer"><router-link to="/about">About Me</router-link></li>
             <li class="my-2 mr-8 text-white cursor-pointer"><router-link to="/contact">Get In Touch</router-link></li>
@@ -49,6 +49,9 @@ export default {
     methods: {
         toggleMenu() {
             this.menuOpen = !this.menuOpen
+        },
+        closeMenu() {
+            this.menuOpen = false
         }
     }
 }
